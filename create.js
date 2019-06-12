183f6835b2e239044825b6ae82d1ca9d83932ee1
Luca <bessilu.de@gmail.com>
2019-06-12 19:27:46 +0200

    pics for slider

--- create.js
+++ create.js
@@ -22,3 +22,15 @@ $(document).ready(function () {
         });
 
 });
+
+var imageUrl = new Array(156);
+
+for(var i=0;i<156;i++){ 
+imageUrl[i] = 'assets/renders/0%20(' + i + ').jpg'; }
+
+       
+$(document).on('input change', '#slider', function() {//listen to slider changes
+    var v=$(this).val();//getting slider val
+   $('#sliderStatus').html( $(this).val() );
+  $("#renders").prop("src", imageUrl[v]);
+});
